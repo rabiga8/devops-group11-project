d06cc78561b0a3e98bcee8091c3f661b2c492e9b
rabiga <49576033+rabiga8@users.noreply.github.com>
2024-04-15 14:32:25 -0400

    Update SpringProjectApplication.java - hello world

--- src/main/java/com/example/springproject/SpringProjectApplication.java
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringProjectApplication.class, args);
+		//SpringApplication.run(SpringProjectApplication.class, args);
+		System.out.println("Hello, world!");
 	}
 
 }
